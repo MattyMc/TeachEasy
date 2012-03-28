a4d23e1a0cf84a17d6495bdddcda8d9223dac5fd
Matt McInnis <mcinnis.matt@gmail.com>
2012-03-28 19:06:13 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teacheasy::Application.config.secret_token = '110d928f47f1f70ae040a9a326d91f192253de327f2d4746e0b6031435e841a24c4361d79e2243fbbe04e355e5df128eb92e712a04c442fb13ec5e012fd69fb5'
